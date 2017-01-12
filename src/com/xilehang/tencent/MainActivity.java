7b92166ebc7a49d573c988ffe8f5130af9fa3755
kristch <wwq2862108920@163.com>
2017-01-12 12:41:11 +0800

    添加全屏无标题

--- src/com/xilehang/tencent/MainActivity.java
+++ src/com/xilehang/tencent/MainActivity.java
@@ -17,6 +17,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.view.MotionEvent;
 import android.view.View.OnTouchListener;
@@ -60,6 +61,8 @@ public class MainActivity extends Activity implements OnItemSelectedListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main);
 		init3dGallery();// µ×²¿ÂÖ²¥
 
